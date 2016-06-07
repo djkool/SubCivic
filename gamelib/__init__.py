4c2f4cd49703fa255a63218c7614ce6c793ec6c3
djkool <andrewpeterson86@gmail.com>
2016-06-06 23:00:23 -0400

    Add gamelib package.
    These modules provide various game oriented functionality conveniently abstracted from this particular game.

---
+++ gamelib/__init__.py
@@ -0,0 +1,8 @@
+"""
+gamelib - Package of generic modules geared towards game development.
+
+Modules:
+game.py		- Provides a basic framework for game looping and states.
+asset.py	- Basic Asset Management including Tilesets and Animations.
+primitives.py - Collection of geometric primitives and their mathematical interactions.
+"""
